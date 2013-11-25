e3db1a828c2f9df8b4b12aa81d5dd0e8b4cfd033
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-11-25 23:12:02 +0200

    - update manager added

---
+++ SofiaPublicTransport/SPTUpdateManager.m
@@ -0,0 +1,13 @@
+//
+//  SPTUpdateManager.m
+//  SofiaPublicTransport
+//
+//  Created by Ivelin Ivanov on 11/25/13.
+//  Copyright (c) 2013 Ivelin Ivanov. All rights reserved.
+//
+
+#import "SPTUpdateManager.h"
+
+@implementation SPTUpdateManager
+
+@end
